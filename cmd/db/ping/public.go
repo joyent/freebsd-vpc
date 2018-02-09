6b1e7d6362c58a44cd03920cc9d8d1bf1ad7137d
Sean Chittenden <sean@chittenden.org>
2018-02-08 19:15:36 -0800

    Actually implement the `db ping` command.

--- cmd/db/ping/public.go
+++ cmd/db/ping/public.go
@@ -1,10 +1,13 @@
 package ping
 
 import (
-	"fmt"
+	"time"
 
+	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
-	"github.com/sean-/conswriter"
+	"github.com/sean-/vpc/agent"
+	"github.com/sean-/vpc/config"
+	"github.com/sean-/vpc/internal/buildtime"
 	"github.com/sean-/vpc/internal/command"
 	"github.com/spf13/cobra"
 )
@@ -16,55 +19,35 @@ var Cmd = &command.Command{
 		SilenceUsage: true,
 		Args:         cobra.NoArgs,
 		PreRunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Printf("here\n")
 			return nil
 		},
 
 		RunE: func(cmd *cobra.Command, args []string) error {
-			cons := conswriter.GetTerminal()
+			log.Info().Str("command", "ping").Msg("")
 
-			fmt.Fprintf(cons, "start\n")
-			for i := 0; i < 100; i++ {
-				fmt.Fprintf(cons, "output\n")
-				log.Info().Msg("output")
+			// 1. Parse config
+			cfg, err := config.New()
+			if err != nil {
+				return errors.Wrap(err, "unable to load configuration")
 			}
-			fmt.Fprintf(cons, "end\n")
 
-			// tritonClientConfig, err := api.InitConfig()
-			// if err != nil {
-			// 	return err
-			// }
+			if err := cfg.Load(); err != nil {
+				return errors.Wrapf(err, "unable to load %s config", buildtime.PROGNAME)
+			}
 
-			// client, err := tritonClientConfig.GetComputeClient()
-			// if err != nil {
-			// 	return err
-			// }
+			// 2. Run agent
+			a, err := agent.New(cfg)
+			if err != nil {
+				return errors.Wrapf(err, "unable to create a new %s agent", buildtime.PROGNAME)
+			}
 
-			// instances, err := client.Instances().List(context.Background(), &compute.ListInstancesInput{})
-			// if err != nil {
-			// 	return err
-			// }
-
-			// table := tablewriter.NewWriter(cons)
-			// table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
-			// table.SetHeaderLine(false)
-			// table.SetAutoFormatHeaders(true)
-
-			// table.SetColumnAlignment([]int{tablewriter.ALIGN_LEFT, tablewriter.ALIGN_RIGHT, tablewriter.ALIGN_RIGHT})
-			// table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
-			// table.SetCenterSeparator("")
-			// table.SetColumnSeparator("")
-			// table.SetRowSeparator("")
-
-			// table.SetHeader([]string{"id", "name", "image", "package"})
-
-			// var numInstances uint
-			// for _, instance := range instances {
-			// 	table.Append([]string{instance.ID, instance.Name, instance.Image, instance.Package})
-			// 	numInstances++
-			// }
-
-			// table.Render()
+			// 3. Connect to the database to verify database credentials
+			start := time.Now()
+			if err := a.Pool().Ping(); err != nil {
+				return errors.Wrap(err, "unable to ping database")
+			}
+			elapsed := time.Now().Sub(start)
+			log.Info().Str("duration", elapsed.String()).Msg("ping")
 
 			return nil
 		},
