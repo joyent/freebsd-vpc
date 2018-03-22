744749f304d44d277c55abf088e723f07dd98223
James Nugent <james@jen20.com>
2018-03-22 17:44:27 +0000

    meta: kldload vmmnet in Vagrant

--- vagrant/scripts/vagrant-freebsd-priv-bhyve.sh
+++ vagrant/scripts/vagrant-freebsd-priv-bhyve.sh
@@ -8,6 +8,7 @@ fi
 
 echo 'net.link.tap.up_on_open=1' >> /etc/sysctl.conf
 sysrc -f /boot/loader.conf 'vmm_load=YES'
+sysrc -f /boot/loader.conf 'vmmnet_load=YES'
 sysrc -f /boot/loader.conf 'nmdm_load=YES'
 sysrc -f /boot/loader.conf 'if_bridge_load=YES'
 sysrc -f /boot/loader.conf 'if_tap_load=YES'
