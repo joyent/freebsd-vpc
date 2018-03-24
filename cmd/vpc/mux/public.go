1168ae045e8232360d615d8958369c09326085db
Sean Chittenden <sean@chittenden.org>
2018-03-24 16:02:16 -0700

    Stub out the `vpc mux` subcommand
    Fixes: #41

---
+++ cmd/vpc/mux/public.go
@@ -0,0 +1,34 @@
+package mux
+
+import (
+	"github.com/joyent/freebsd-vpc/internal/command"
+	"github.com/pkg/errors"
+	"github.com/spf13/cobra"
+)
+
+const cmdName = "mux"
+
+var Cmd = &command.Command{
+	Name: cmdName,
+
+	Cobra: &cobra.Command{
+		Use:   cmdName,
+		Short: "VPC packet multiplexing configuration",
+	},
+
+	Setup: func(self *command.Command) error {
+		subCommands := command.Commands{
+			//create.Cmd,
+			//connect.Cmd,
+			//disconnect.Cmd,
+			//show.Cmd,
+			//destroy.Cmd,
+		}
+
+		if err := self.Register(subCommands); err != nil {
+			return errors.Wrapf(err, "unable to register sub-commands under %s", cmdName)
+		}
+
+		return nil
+	},
+}
